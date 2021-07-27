fdfd99ab0cd258b5448ff39f42a3d5b0a6911e45
include-istream <59331347+include-istream@users.noreply.github.com>
2021-07-27 10:38:14 +0800

    Update TestOfST.cpp

--- TestOfST.cpp
+++ TestOfST.cpp
@@ -1,16 +1,14 @@
 #include <iostream>
 #include "SegmentTree.h"
-#pragma comment(linker,"/STACK:2147483648")
 using namespace std;
 int main()
 {
-	int n;
-	cin >> n;
-	SegmentTree<int> a(n);
-	cout << a.intervalSum(1, 1, n, 1, n) << endl;
-	int k;
-	cin >> k;
-	a.modifyInPd(1, 1, n, 2, n - 1, k);
-	cout << a.queryInPd(1, 1, n, 1, n);
+	SegmentTree<int> a(5);
+	cout << a.intervalSum(1, 5, 2, 4)<<endl;
+	a.modifyInPd(1, 5, 2, 4, 10);    
+	cout << a.queryInPd(1, 5, 2, 4)<<endl;
+	a.modifyInPd(1, 5, 2, 4, 10);
+	cout << a.queryInPd(1, 5, 2, 4)<<endl; 
+	
 	return 0;
 }
