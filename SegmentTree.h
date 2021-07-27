1b4e8fcc0d3f51b1aeb118ddc526708c51a7fb26
include-istream <59331347+include-istream@users.noreply.github.com>
2021-07-27 10:25:12 +0800

    Update SegmentTree.h

--- SegmentTree.h
+++ SegmentTree.h
@@ -2,48 +2,52 @@
 
 #include <iostream>
 #include <cstdio>
-constexpr auto MAXN = 500010;
-//#pragma comment(linker, "/STACK:2147483648")
+#include <vector>
+//constexpr auto MAXN = 5010;                
+//#pragma comment(linker, "/STACK:20480")    If your programme has stack limit, please add this sentence. You can change the limit.
 using std::cin;
 using std::cout;
 using std::endl;
 using std::min;
 using std::max;
+using std::vector;
 
 template<class T>
 class SegmentTree
 {
 private:
-	T sum[MAXN];
-	T add[MAXN];
+	vector<T> sum;
+	vector<T> add;
 public:
-	void build(int tree_num, int left, int right, int array_num, T value);
-	T intervalSum(int tree_num, int left, int right, int x, int y);
-	T query(int tree_num, int left, int right, int x, int y);                   //query() returns the value in Σ[x,y]
-	void modify(int tree_num, int left, int right, int x, int y, T value);      //modify() changes the value in [x,y]
+	void build(int left, int right, int array_num, T value,int tree_num=1);
+	T intervalSum(int left, int right, int x, int y,int tree_num=1);
+	T query(int left, int right, int x, int y,int tree_num=1);                   //query() returns the value in Σ[x,y]
+	void modify(int left, int right, int x, int y, T value,int tree_num=1);      //modify() changes the value in [x,y]
 
-	void flagModify(int tree_num, int left, int right, T value);
-	void pushdown(int tree_num, int left, int right, int mid);
-	void modifyInPd(int tree_num, int left, int right, int x, int y, T value);
-	T queryInPd(int tree_num, int left, int right, int x, int y);
+	void flagModify(int left, int right, T value,int tree_num=1);                
+	void pushdown(int left, int right, int mid, int tree_num=1);                 
+	void modifyInPd(int left, int right, int x, int y, T value, int tree_num=1);
+	T queryInPd(int left, int right, int x, int y,int tree_num=1);
 	SegmentTree() {};
 	SegmentTree(int n);
-	//~SegmentTree();
+	//~SegmentTree() {};
 };
 
 template<class T>
-SegmentTree<T>::SegmentTree(int n)
+SegmentTree<T>::SegmentTree(int n): sum(4 * T(n), 0),add(4 * T(n), 0)
 {
+	cout << "Please enter " << n << " numbers to build Segment Tree." << endl;
 	for (int i = 1; i <= n; i++)
 	{
 		T v = 0;
 		cin >> v;
-		build(1, 1, n, i, v);
+		build(1, n, i, v);
 	}
+	cout << "The initialization of Segment Tree is successful!" << endl;
 }
 
 template<class T>
-void SegmentTree<T>::build(int tree_num, int left, int right, int array_num, T value)
+void SegmentTree<T>::build(int left, int right, int array_num, T value, int tree_num)
 {
 	if (array_num < left || array_num > right) return;
 	if (left == right && left == array_num)
@@ -52,30 +56,30 @@ void SegmentTree<T>::build(int tree_num, int left, int right, int array_num, T v
 		return;
 	}
 	int mid = (left + right) >> 1;
-	build(2 * tree_num, left, mid, array_num, value);
-	build(2 * tree_num + 1, mid + 1, right, array_num, value);
-	sum[tree_num] = sum[tree_num * 2] + sum[tree_num * 2 + 1];
+	build(left, mid, array_num, value, 2 * tree_num);
+	build(mid + 1, right, array_num, value, 2 * tree_num + 1);
+	sum[tree_num] = sum[T(tree_num) * 2] + sum[T(tree_num) * 2 + 1];
 }
 //This function adds a node to the tree. And it's number in the array is array_num,
 //value is value, number in the tree is tree_num.
 
 
 template<class T>
-T SegmentTree<T>::intervalSum(int tree_num, int left, int right, int x, int y)
+T SegmentTree<T>::intervalSum(int left, int right, int x, int y, int tree_num)
 {
 	int mid = 0;
 	T res = 0;
 	if (x > right || y < left) return 0;
 	if (x <= left && y >= right) return sum[tree_num];
 	mid = (left + right) >> 1;
-	res = query(2 * tree_num, left, mid, x, y);
-	res += query(2 * tree_num + 1, mid + 1, right, x, y);
+	res = query(left, mid, x, y, 2 * tree_num);
+	res += query(mid + 1, right, x, y, 2 * tree_num + 1);
 	return res;
 }
 //This function is used for interval sum.
 
 template<class T>
-void SegmentTree<T>::modify(int tree_num, int left, int right, int x, int y, T value)
+void SegmentTree<T>::modify(int left, int right, int x, int y, T value, int tree_num)
 {
 	if (x > right || y < left) return;
 	if (left >= x && right <= y)
@@ -85,21 +89,21 @@ void SegmentTree<T>::modify(int tree_num, int left, int right, int x, int y, T v
 	}
 	sum[tree_num] += (min(right, y) - max(left, x) + 1) * value; //[left,right]∩[x,y]+1
 	int mid = (left + right) >> 1;
-	if (x <= mid) modify(2 * tree_num, left, mid, x, y, value);
-	if (mid < y) modify(2 * tree_num + 1, mid + 1, right, x, y, value);
+	if (x <= mid) modify(left, mid, x, y, value, 2 * tree_num);
+	if (mid < y) modify(mid + 1, right, x, y, value, 2 * tree_num + 1);
 }
 //This function adds the value to a interval [x,y]. 
 
 template<class T>
-T SegmentTree<T>::query(int tree_num, int left, int right, int x, int y)
+T SegmentTree<T>::query(int left, int right, int x, int y, int tree_num)
 {
 	if (x > right || y < left) return 0;  //This sentence is VERY IMPROTANT!!!DON'T FORGET TO WRITE IT!!!
 	if (left >= x && right <= y) return  sum[tree_num] + (T(right) - T(left) + 1) * add[tree_num];
 	int mid = (left + right) >> 1;
 	T res = 0;
 	res = (T(min(right, y)) - T(max(left, x)) + 1) * add[tree_num];
-	if (x <= mid) res += query(2 * tree_num, left, mid, x, y);
-	if (mid < y) res += query(2 * tree_num + 1, mid + 1, right, x, y);
+	if (x <= mid) res += query(left, mid, x, y, 2 * tree_num);
+	if (mid < y) res += query(mid + 1, right, x, y, 2 * tree_num + 1);
 	return res;
 }
 //This function has the same function as intervalSum() while it's used for interval change.
@@ -109,7 +113,7 @@ T SegmentTree<T>::query(int tree_num, int left, int right, int x, int y)
 
 
 template<class T>
-void SegmentTree<T>::flagModify(int tree_num, int left, int right, T value)
+void SegmentTree<T>::flagModify(int left, int right, T value, int tree_num)
 {
 	add[tree_num] += value;
 	sum[tree_num] += (T(right) - T(left) + 1) * value;
@@ -118,37 +122,37 @@ void SegmentTree<T>::flagModify(int tree_num, int left, int right, T value)
 //This function signs an add flag(array add) instead of really add value to the node.
 
 template<class T>
-void SegmentTree<T>::pushdown(int tree_num, int left, int right, int mid)
+void SegmentTree<T>::pushdown(int left, int right, int mid, int tree_num)
 {
 	if (add[tree_num] == 0) return;
-	flagModify(2 * tree_num, left, mid, add[tree_num]);
-	flagModify(2 * tree_num + 1, mid + 1, right, add[tree_num]);
+	flagModify(left, mid, add[tree_num], 2 * tree_num);
+	flagModify(mid + 1, right, add[tree_num], 2 * tree_num + 1);
 	add[tree_num] = 0;
 }
 //This function push down a node's flag.It's left child and right child will get this flag if their flag is 0.
 
 template<class T>
-void SegmentTree<T>::modifyInPd(int tree_num, int left, int right, int x, int y, T value)
+void SegmentTree<T>::modifyInPd(int left, int right, int x, int y, T value, int tree_num)
 {
 	if (y < left || x > right) return;
-	if (left >= x && right <= y) return flagModify(tree_num, left, right, value);
+	if (left >= x && right <= y) return flagModify(left, right, value, tree_num);
 	int mid = (left + right) >> 1;
-	pushdown(tree_num, left, right, mid);
-	if (x <= mid) modifyInPd(2 * tree_num, left, mid, x, y, value);
-	if (mid < y) modifyInPd(2 * tree_num + 1, mid + 1, right, x, y, value);
-	sum[tree_num] = sum[2 * tree_num] + sum[2 * tree_num + 1];
+	pushdown(left, right, mid, tree_num);
+	if (x <= mid) modifyInPd(left, mid, x, y, value, 2 * tree_num);
+	if (mid < y) modifyInPd(mid + 1, right, x, y, value, 2 * tree_num + 1);
+	sum[tree_num] = sum[2 * T(tree_num)] + sum[2 * T(tree_num) + 1];
 }
 
 template<class T>
-T SegmentTree<T>::queryInPd(int tree_num, int left, int right, int x, int y)
+T SegmentTree<T>::queryInPd(int left, int right, int x, int y, int tree_num)
 {
 	if (y < left || x > right) return 0;
 	if (left >= x && right <= y) return sum[tree_num];
 	int mid = (left + right) >> 1;
 	T res = 0;
-	pushdown(tree_num, left, right, mid);
-	if (x <= mid) res += queryInPd(2 * tree_num, left, mid, x, y);
-	if (mid < y) res += queryInPd(2 * tree_num + 1, mid + 1, right, x, y);
+	pushdown(left, right, mid, tree_num);
+	if (x <= mid) res += queryInPd(left, mid, x, y, 2 * tree_num);
+	if (mid < y) res += queryInPd(mid + 1, right, x, y, 2 * tree_num + 1);
 	return res;
 }
 
